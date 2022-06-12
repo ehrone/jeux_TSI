8636cf7c29d870844517509c297c60df3152058d
emma begard <emma1.begard@gmail.com>
2022-06-12 14:25:00 +0200

    test

--- main.py
+++ main.py
@@ -112,9 +112,16 @@ def main():
     texture = glutils.load_texture('mur.jpg')
     p =[0,0,0,0,0,0,0,0]
     centre =[0+tr.translation.x , 0+tr.translation.y , 0+tr.translation.z]
-
     obstacle = decors(m.load_to_gpu(), m.get_nb_triangles(), program3d_id, texture, tr ,0,0,0, p, centre)
     viewer.add_object(obstacle) 
+
+    vao = Text.initalize_geometry()
+    texture = glutils.load_texture('fontB.jpg')
+    o = Text('Zebi le', np.array([-0.8, 0.3], np.float32), np.array([0.8, 0.8], np.float32), vao, 2, programGUI_id, texture)
+    viewer.add_object(o)
+    o = Text('Dinosaure', np.array([-0.5, -0.2], np.float32), np.array([0.5, 0.3], np.float32), vao, 2, programGUI_id, texture)
+    viewer.add_object(o)
+
 
     viewer.run()
 
